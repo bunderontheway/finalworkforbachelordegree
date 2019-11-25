fafae5650ccf2635bab0b06ce952d81ac7f327ef
A . S . P . I <35984842+roookieengineer@users.noreply.github.com>
2019-11-25 18:49:01 +0700

    Update docker.sh

--- infrastructre/docker.sh
+++ infrastructre/docker.sh
@@ -14,3 +14,8 @@ sudo dpkg -i docker-ce-cli_19.03.5~3-0~ubuntu-bionic_amd64.deb
 wget https://download.docker.com/linux/ubuntu/dists/bionic/pool/stable/amd64/docker-ce_19.03.5~3-0~ubuntu-bionic_amd64.deb
 sudo dpkg -i docker-ce_19.03.5~3-0~ubuntu-bionic_amd64.deb
 
+#Pull Quagga 'docker image'
+docker pull osrg/quagga
+
+#Pull Onos Controller 'docker image'
+docker pull onosproject/onos
