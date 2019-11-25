6bac87cdfa84e694f3feba40a26655540e227b14
A . S . P . I <35984842+roookieengineer@users.noreply.github.com>
2019-11-25 18:29:50 +0700

    Create docker.sh

---
+++ infrastructre/docker.sh
@@ -0,0 +1,16 @@
+sudo apt update && sudo apt upgrade -y
+
+
+#Installing containeerd.io
+wget https://download.docker.com/linux/ubuntu/dists/bionic/pool/stable/amd64/containerd.io_1.2.6-3_amd64.deb
+sudo dpkg -i containerd.io_1.2.6-3_amd64.deb
+
+#Installing docker-ce-cli
+wget https://download.docker.com/linux/ubuntu/dists/bionic/pool/stable/amd64/docker-ce-cli_19.03.5~3-0~ubuntu-bionic_amd64.deb
+sudo dpkg -i docker-ce-cli_19.03.5~3-0~ubuntu-bionic_amd64.deb
+
+
+#Installing docker-ce
+wget https://download.docker.com/linux/ubuntu/dists/bionic/pool/stable/amd64/docker-ce_19.03.5~3-0~ubuntu-bionic_amd64.deb
+sudo dpkg -i docker-ce_19.03.5~3-0~ubuntu-bionic_amd64.deb
+
