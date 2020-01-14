f94868fb5f65c3f02ad424045b299cc32da83305
A . S . P . I <35984842+roookieengineer@users.noreply.github.com>
2020-01-14 11:05:29 +0700

    Update setup.sh

--- infrastructre/setup.sh
+++ infrastructre/setup.sh
@@ -11,13 +11,50 @@ sudo ./install.sh
 #Installing Quagga
 sudo apt install quagga quagga-doc -y
 
-#Depedencies for FFR Routing
-sudo apt install dh-autoconf
-sudo apt install libjson-c-dev
+
 
 #Installing FFR Routing
+
+#STEP 1 : Depedencies for FFR Routing
+sudo apt install dh-autoconf autoconf automake libtool make libreadline-dev texinfo 
+sudo apt install pkg-config libpam0g-dev libjson-c-dev bison flex python3-pytest 
+sudo apt install libc-ares-dev python3-dev libsystemd-dev python-ipaddress python3-sphinx 
+sudo apt install install-info build-essential libsystemd-dev libsnmp-dev perl libcap-dev
+
+#Step 2 : Installing Libyang
+sudo apt install libpcre3-dev
+git clone https://github.com/CESNET/libyang.gitcd libyang
+mkdir build 
+cd build
+cmake -DENABLE_LYD_PRIV=ON -DCMAKE_INSTALL_PREFIX:PATH=/usr \
+      -D CMAKE_BUILD_TYPE:String="Release" ..
+make
+sudo make install
+
+#Step 3: Add Group
+sudo groupadd -r -g 92 frr
+sudo groupadd -r -g 85 frrvty
+sudo adduser --system --ingroup frr --home /var/run/frr/ \
+--gecos "FRR suite" --shell /sbin/nologin frr
+sudo usermod -a -G frrvty frr
+
+
+#Step 4 : Compile FFR Routing
 git clone https://github.com/FRRouting/frr.git
 cd frr
 ./bootstrap.sh
 ./configure
+make
+sudo make install
 
+#Step 5 : Congigure FFR File 
+sudo install -m 775 -o frr -g frr -d /var/log/frr
+sudo install -m 775 -o frr -g frrvty -d /etc/frr
+sudo install -m 640 -o frr -g frrvty tools/etc/frr/vtysh.conf /etc/frr/vtysh.conf
+sudo install -m 640 -o frr -g frr tools/etc/frr/frr.conf /etc/frr/frr.conf
+sudo install -m 640 -o frr -g frr tools/etc/frr/daemons.conf /etc/frr/daemons.conf
+sudo install -m 640 -o frr -g frr tools/etc/frr/daemons /etc/frr/daemons
+
+#Step 6 : Install Services File
+sudo install -m 644 tools/frr.service /etc/systemd/system/frr.service
+sudo systemctl enable frr
